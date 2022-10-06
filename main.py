a6656adb7a7edab43ecd34a6a4b581d8d94364a3
awx1192780 <ao.sun@huawei.com>
2022-10-06 18:13:38 +0200

    新增同时导出交付件功能 V1.1.0

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import json
 import configparser
 import inspect
 import collections
+import re
 import pandas as pd
 import openpyxl
 from openpyxl import Workbook
@@ -26,7 +27,7 @@ config = configparser.ConfigParser()
 config.read('./confs/conf.ini', encoding='UTF-8')
 current_year_and_month = time.strftime("%Y%m", time.localtime())
 # 手动修改：
-# current_year_and_month = "202209"
+current_year_and_month = "202209"
 #####
 current_year = current_year_and_month[:-2]
 EVENT = str(config['smartwork']["event"])
@@ -43,7 +44,9 @@ BASE_FOLDER = str(config['smartwork']["base_folder"])
 REPORT_FOLDER = str(config['smartwork']["report_folder"])
 REPORT_PATH = '{}\\{}交付件.xlsx'.format(REPORT_FOLDER, current_year)
 SHEET_NAME = '{}月'.format(int(current_year_and_month[-2:]))
+REPORT_SHEET_NAME = '{}月-交付件'.format(int(current_year_and_month[-2:]))
 HEADER = json.loads(config['smartwork']["header"])
+INFO_HEADER = json.loads(config['smartwork']['info_header'])
 keys_list = [EVENT, TROUBLESHOOTING, PM, ALARM, VERSION_UPDATE,
              VERSION_UPDATE_MEETING, ACTIVE_OPERATION_MAINTAIN, FAQ, NEGATIVE_EVENT]
 
@@ -60,25 +63,70 @@ def copy_dict_from(source_dict: dict):
             target_dict[i] = 0
     return target_dict
 
-# 统计
+# 统计数量
 
 
-def do_count(smart_dict, path):
+def do_count(smart_dict, report_dict, path):
     if '.txt' not in path:
         return None
-    file = open(path, 'r', encoding='UTF-8')
+    _file = open(path, 'r', encoding='UTF-8')
 
+    # 检索当前工作日志
     def verify(keys_list, line):
         for i in keys_list:
             if retrieve_name(i)[0] not in smart_dict:
                 smart_dict[retrieve_name(i)[0]] = 0
             if line.find(i) != -1:
                 return retrieve_name(i)[0]
-    for line in file:
+    # 生成交付件
+    _re = '.*工作日报(\d+).txt'
+    cur_date = re.search(_re, path).group(1)
+    cur_date = '{}/{}/{}'.format(cur_date[:4], cur_date[4:6], cur_date[6:8])
+    if_record = False
+    _contents = []
+    descs = []
+    lines = _file.readlines()
+    _len = len(lines)
+    _report_dict = {}
+    for _index, line in enumerate(lines):
+        _list = []
         res = verify(keys_list, line)
-        if res:
-            smart_dict[res] += 1
-    return smart_dict
+        _content = re.sub(r'\d\.?(.*】)?(.*\w)[\.:：。]?', r'\2', line).strip()
+        type_group = re.search('【(.*)】', line)
+        if re.sub('\d{1,2}\.', '', line).strip() != '':
+            if re.match('\s?\d{1,2}\.', line) != None and type_group != None:
+                _type = type_group.group(1)
+                _report_dict[_content] = [_type, cur_date, 2]
+                # 记录当前内容
+                if_record = True
+                if len(descs) != 0:
+                    _contents.append(descs)
+                descs = []
+            elif re.match('\s?\d{1,2}\.', line) != None and type_group == None:
+                # 不记录当前内容
+                if_record = False
+                if len(descs) != 0:
+                    _contents.append(descs)
+                descs = []
+            else:
+                if if_record and _index != _len-1:
+                    descs.append(
+                        re.sub('【.*】', '', line.replace('\t', '').replace('- ', "◊ ")))
+            if res:
+                smart_dict[res] += 1
+    if len(descs) != 0:
+        _contents.append(descs)
+    if len(_contents) < len(_report_dict):
+        _contents.extend([[] for i in range(len(_report_dict)-len(_contents))])
+    for _index, item in enumerate(_report_dict):
+        _report_dict[item].append('{}'.format(''.join(_contents[_index])))
+    for k, v in _report_dict.items():
+        v.insert(0, k)
+    report_dict = {**report_dict, **_report_dict}
+    _contents = []
+    # print('report',report_dict)
+    return smart_dict, report_dict
+
 
 # 导出Excel
 
@@ -90,31 +138,46 @@ def to_excel(smart_dict):
         processed_data = df.values.tolist()
         if os.path.exists(REPORT_FOLDER) == False:
             os.makedirs(REPORT_FOLDER)
-        print('[processed_data] {} TO {}'.format(json.dumps(dict(zip(HEADER, one_layer(
-            processed_data))), indent=4, ensure_ascii=False), str(REPORT_PATH)))
-        export_to_excel(str(REPORT_PATH), HEADER, processed_data, SHEET_NAME)
+        print('[processed_data] {} TO {}  => [{}]'.format(json.dumps(dict(zip(HEADER, one_layer(
+            processed_data))), indent=4, ensure_ascii=False), str(REPORT_PATH), SHEET_NAME))
+        export_to_excel(str(REPORT_PATH), HEADER, processed_data, SHEET_NAME, data_type='count')
     except Exception as e:
         print("导出失败，不存在目录或文件正在被使用。", e)
 
+def to_report_excel(report_dict, spec_column):
+    try:
+            df = pd.DataFrame(list(report_dict.values()), columns=[INFO_HEADER])
+            processed_data = df.values.tolist()
+            if os.path.exists(REPORT_FOLDER) == False:
+                os.makedirs(REPORT_FOLDER)
+            print('[report_data] {} TO {} => [{}]'.format(json.dumps(dict(zip(INFO_HEADER, one_layer(
+                processed_data))), indent=4, ensure_ascii=False), str(REPORT_PATH), REPORT_SHEET_NAME))
+            export_to_excel(str(REPORT_PATH), INFO_HEADER,
+                            processed_data, REPORT_SHEET_NAME, 'report', spec_column)
+    except Exception as e:
+        print("导出失败，不存在目录或文件正在被使用。", e)
 # 文件夹检索
 
 
 def traverse():
     smart_dict = collections.defaultdict(int)
+    res_dict = {}
     if os.path.exists(BASE_FOLDER) == False:
         print('不存在此目录:{}'.format(BASE_FOLDER))
         os._exit(0)
-    for dirname in tqdm(os.listdir(BASE_FOLDER), desc='搜索中'):
+    for dirname in tqdm(os.listdir(BASE_FOLDER), desc='输出报告', position=0):
         dirpath = '{}\\{}'.format(BASE_FOLDER, dirname)
         if current_year_and_month in dirname and os.path.isdir(dirpath):
             for report in os.listdir(dirpath):
                 if '.txt' in report:
                     file_path = '{}\\{}'.format(
                         dirpath, report).replace('\\', '\\\\')
-                    smart_dict = do_count(smart_dict, file_path)
-    return smart_dict
+                    smart_dict, res_dict = do_count(
+                        smart_dict, res_dict, file_path)
+    return smart_dict, res_dict
 
 
 if __name__ == '__main__':
-    smart_dict = traverse()
+    smart_dict, res_dict = traverse()
     to_excel(smart_dict)
+    to_report_excel(res_dict,spec_column="E")
--- util/excel_util.py
+++ util/excel_util.py
@@ -10,7 +10,8 @@
 
 from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
-from openpyxl.styles import Alignment
+from openpyxl.styles import Alignment, Font, Border, Side
+from tqdm import tqdm
 import os
 import copy
 import time
@@ -18,27 +19,105 @@ import time
 
 __all__ = ['open_file', 'open_or_add_sheet', 'data_excel', 'export_to_excel']
 
+
 # 批量插入支持传入'A1' 和 [row,col]形式的数据
 # 批量插入数据
 # mode 0 横向 1 纵向
 
 
-def batch_insert(ws, _list, pos=[1, 1], mode=0):
+def batch_insert(ws, _list, pos=[1, 1], spec_column=None, mode=0, _vertical='center'):
     row, column = 1, 1
     if isinstance(pos, str):
-        row, column = [ord(str(pos[0:1]).upper())-65+1, int(pos[1:])]
+        row, column = [ord(letter)-65+1, int(pos[1:])]
     if isinstance(pos, list):
         row, column = pos
     if mode == 0:
         for index, value in enumerate(_list):
-            ws.cell(row=row, column=column+index,
-                    value=value).alignment = Alignment(horizontal='center', vertical='center')
+            _target_cell = ws.cell(row=row, column=column+index,
+                                   value=value)
+            _target_cell.alignment = Alignment(
+                horizontal='left', vertical='top', wrapText=True)
+            thin = Side(border_style="thin", color="000000")
+            double = Side(border_style="double", color="000000")
+            _target_cell.border = Border(
+                top=double, left=thin, right=thin, bottom=double)
+            # 微调格式
+
+            if row == 1:
+                ws.cell(row=row, column=column+index).font = Font(bold=True)
+
             # 居中处理
+            # wrapText=True 自动换行
+
     else:
         for index, value in enumerate(_list):
             ws.cell(row=row+index, column=column,
-                    value=value).alignment = Alignment(horizontal='center', vertical='center')
+                    value=value).alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
     return ws
+
+
+def batch_insert_report(ws, _list, pos=[1, 1], spec_column=None, mode=0, _vertical='center'):
+    row, column = 1, 1
+    if isinstance(pos, str):
+        row, column = [ord(letter)-65+1, int(pos[1:])]
+    if isinstance(pos, list):
+        row, column = pos
+    smart_width_and_height(ws, row, _list)
+    if spec_column != None:
+        ws.column_dimensions[spec_column].width = 105
+    if mode == 0:
+        for index, value in enumerate(_list):
+            _target_cell = ws.cell(row=row, column=column+index,
+                                   value=value)
+            _target_cell.alignment = Alignment(
+                horizontal='center', vertical='center', wrapText=True)
+            thin = Side(border_style="thin", color="000000")
+            double = Side(border_style="double", color="000000")
+            _target_cell.border = Border(
+                top=double, left=thin, right=thin, bottom=double)
+            # 微调格式
+            if row == 1:
+                ws.cell(row=row, column=column+index).font = Font(bold=True)
+            if index == len(_list) -1 :
+                ws.cell(row=row, column=column+index).alignment = Alignment(
+                    horizontal='left', vertical='top', wrapText=True)
+            ws.cell(row=1, column=len(_list)).alignment = Alignment(
+                    horizontal='center', vertical='center', wrapText=True)
+
+            # 居中处理
+            # wrapText=True 自动换行
+
+    else:
+        for index, value in enumerate(_list):
+            ws.cell(row=row+index, column=column,
+                    value=value).alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
+    return ws
+
+
+# 智能调整单元格宽度和高度
+
+
+def smart_width_and_height(ws, _row, _list: list):
+    for _index, _ in enumerate(_list):
+        ws.column_dimensions[chr(_index+65).upper()].width = 25
+    # 根据content和desc中的内容调整单元格高度
+    tar_height = 28 + 4 * (int(len(_list[0])/9)+1) + \
+        ''.join(map(str, _list)).count('\n') * 15
+    ws.row_dimensions[_row].height = tar_height
+
+
+def set_filter_and_sort(ws, _all_list: list):
+    _row = len(_all_list)
+    _column = len(_all_list[0])
+    # _dates = set([i[2] for i in _all_list])
+    # _times = set([i[3] for i in _all_list])
+    # print(_row,_column,_dates,_times)
+    for _index, _ in enumerate(_all_list):
+        ws.column_dimensions[chr(_index+65).upper()].width = 25
+    if _index not in [0, 4]:
+        ws.auto_filter.add_filter_column(_index, _all_list[_index])
+    ws.auto_filter.ref = "A1:{}1".format(chr(_column+65-1).upper())
+    ws.auto_filter.add_sort_condition("C2:D{}".format(_row))
 
 
 def open_file(excel_name_path):
@@ -61,16 +140,20 @@ def open_or_add_sheet(workbook, sheet_name):
 # 执行插入数据
 
 
-def data_excel(excel_name_path, workbook, worksheet, title, data):
+def data_excel(excel_name_path, workbook, worksheet, title, data, data_type="count", spec_column=None):
     datain = copy.copy(data)  # 浅拷贝
     if isinstance(datain, dict):
         _, values = zip(*datain.items())
     if isinstance(datain, list):
         values = datain
     value_normal = values
-    worksheet = batch_insert(worksheet, title, [1, 1])
+    worksheet = batch_insert(worksheet, title, [1, 1], spec_column) if data_type == 'count' else batch_insert_report(
+        worksheet, title, [1, 1], spec_column)
     for index, value in enumerate(value_normal):
-        worksheet = batch_insert(worksheet, value, [2+index, 1])
+        worksheet = batch_insert(worksheet, value, [
+                                 2+index, 1], spec_column) if data_type == 'count' else batch_insert_report(worksheet, value, [2+index, 1], spec_column)
+    # 暂无法使用filter和sort
+    # set_filter_and_sort(worksheet, value_normal)
     b = workbook.save(excel_name_path)
     workbook.close()
     return worksheet
@@ -78,7 +161,8 @@ def data_excel(excel_name_path, workbook, worksheet, title, data):
 # 主函数
 
 
-def export_to_excel(path, title, data, sheet_name='Default'):
+def export_to_excel(path, title, data, sheet_name='Default', data_type="count", spec_column=None):
     wb = open_file(path)
     ws = open_or_add_sheet(wb, sheet_name)
-    data_excel(path, wb, ws, title, data)
+    data_excel(path, wb, ws, title, data,
+               data_type=data_type, spec_column=spec_column)
